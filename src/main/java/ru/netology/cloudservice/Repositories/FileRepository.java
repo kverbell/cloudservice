4fa3a33c64c1ded913f444395cd43ec05a4be385
Anna Savrova <annsavrova@gmail.com>
2024-09-07 21:59:30 +0700

    Created repositories for database operations, entity classes and schema initialization script
    - Added `FileDataRepository` and `UserRepository` for CRUD operations on `FileData` and `User` entities respectively.
    - Created `FileData` and `User` entity classes with appropriate JPA annotations.
    - Added schema creation SQL script for initializing the SQLite database with required tables.

--- src/main/java/ru/netology/cloudservice/Repositories/FileRepository.java
+++
@@ -1,4 +0,0 @@
-package ru.netology.cloudservice.Repositories;
-
-public class FileRepository {
-}
--- src/main/java/ru/netology/cloudservice/Repositories/UserRepository.java
+++ src/main/java/ru/netology/cloudservice/Repositories/UserRepository.java
@@ -1,4 +1,11 @@
 package ru.netology.cloudservice.Repositories;
 
-public class UserRepository {
+import ru.netology.cloudservice.Entity.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByLogin(String login);
 }
